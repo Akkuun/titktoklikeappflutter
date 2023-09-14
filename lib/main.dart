e49669e0b30605d96ee4702f2c1052a5bb6edb0d
unknown <mathis.duban@etu.umontpellier.fr>
2023-09-14 20:41:57 +0200

    correct history

--- lib/main.dart
+++ lib/main.dart
@@ -5,45 +5,32 @@ import 'package:video_player/video_player.dart';
 
 void main() {
   runApp(const MyApp());
-  //SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
-
 }
+
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-
-    //informations globale de l'application
     return const MaterialApp(
-      //titre app
       title: 'Flutter TikTok',
-      //enlever debug label
       debugShowCheckedModeBanner: false,
-      //page à afficher, ici notre objet BottomNavigationBarExample
       home: BottomNavigationBarExample(),
     );
   }
 }
-// barre de navigation objet
-class BottomNavigationBarExample extends StatefulWidget {
-  const BottomNavigationBarExample({super.key});
 
+class BottomNavigationBarExample extends StatefulWidget {
+  const BottomNavigationBarExample({Key? key}) : super(key: key);
 
   @override
   State<BottomNavigationBarExample> createState() =>
-
       _BottomNavigationBarExampleState();
 }
 
 class _BottomNavigationBarExampleState
     extends State<BottomNavigationBarExample> {
   int _selectedIndex = 0;
-
-  @override
-  void initState() {
-   // SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
-    super.initState();
-  }
 
   void _onItemTapped(int index) {
     setState(() {
@@ -53,27 +40,16 @@ class _BottomNavigationBarExampleState
 
   @override
   Widget build(BuildContext context) {
-
-    //Scaffold = Fenetre entiere classique
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: [
-      SystemUiOverlay.top
-    ]);
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: [SystemUiOverlay.top]);
     SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
     ));
     return Scaffold(
       extendBody: true,
-    //body de l'objet
-      body: Container(
-        margin: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
-        child: const Center(
-          child: Text('Post'),
-        ),
-      ),
-      //bottonNavigationBar Paramètres
+      body: HomePage(),
       bottomNavigationBar: BottomNavigationBar(
-        //déclaration des objet de la barre de navigation, liste d'objet avec un icon, label
-        items:  <BottomNavigationBarItem>[
+        items: <BottomNavigationBarItem>[
           const BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: 'Home',
@@ -83,7 +59,10 @@ class _BottomNavigationBarExampleState
             label: 'Search',
           ),
           BottomNavigationBarItem(
-            icon: Image.asset('assets/images/addIcon.png',height: 25,),
+            icon: Image.asset(
+              'assets/images/addIcon.png',
+              height: 25,
+            ),
             label: 'Add',
           ),
           const BottomNavigationBarItem(
@@ -95,19 +74,326 @@ class _BottomNavigationBarExampleState
             label: 'Profile',
           ),
         ],
-        //parametre de la navigation barre
         currentIndex: _selectedIndex,
         selectedItemColor: Colors.white,
         unselectedItemColor: Colors.grey,
-        //on montre pas les labels
         showSelectedLabels: false,
         showUnselectedLabels: false,
         onTap: _onItemTapped,
-        //on met ça pour mettre 5 icones
         type: BottomNavigationBarType.fixed,
         backgroundColor: const Color(0xFF141518),
       ),
     );
   }
 }
-//barre de navigation fin
+
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  final List<Map<String, String>> tiktokItems = [
+    {
+      "video": "assets/videos/1.mp4",
+      "comment": "@supervideo_87 \n  \n Cherry Blossom tree #cherry #tree #60FPS #pink",
+      "likeCount": "13K",
+      "commentCount": "462",
+      "shareCount": "126",
+      "avatarImage": "assets/images/photo3.png",
+      "music": "Original Sound - Naturally"
+    },
+    {
+      "video": "assets/videos/2.mp4",
+      "comment": "@fireman_12 \n \n Beautiful Fireplace for christmas #fire #fireplace #60FPS #wood",
+      "likeCount": "2.5K",
+      "commentCount": "360",
+      "shareCount": "180",
+      "avatarImage": "assets/images/photo1.png",
+      "music": "Original Sound - Fireplace Mix"
+    },
+    {
+      "video": "assets/videos/3.mp4",
+      "comment": "@forestmaster \n \n Last rain capured yesturday #60FPS #forest #rain #camera ",
+      "likeCount": "16.3K",
+      "commentCount": "865",
+      "shareCount": "265",
+      "avatarImage": "assets/images/photo4.png",
+      "music": "Original Sound - Rainy Day Vibes"
+    },
+    {
+      "video": "assets/videos/video4.mp4",
+      "comment": "@MathElipse \n \n Los Angles timelapse #city #LA #timelapse #light #night ",
+      "likeCount": "15K",
+      "commentCount": "880",
+      "shareCount": "1.5K",
+      "avatarImage": "assets/images/photo4.png",
+      "music": "Original Sound - City Lights"
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return CarouselSlider(
+      options: CarouselOptions(
+        height: double.infinity,
+        scrollDirection: Axis.vertical,
+        viewportFraction: 1.0,
+      ),
+      items: tiktokItems.map((item) {
+        return Builder(
+          builder: (BuildContext context) {
+            return Container(
+              color: const Color(0xFF141518),
+              child: Stack(
+                children: [
+                  VideoWidget(videoUrl: item['video']!),
+                  PostContent(
+                    comment: item['comment']!,
+                    likeCount: item['likeCount']!,
+                    commentCount: item['commentCount']!,
+                    shareCount: item['shareCount']!,
+                    avatarImage: item['avatarImage']!,
+                    music: item['music']!,
+                  ),
+                ],
+              ),
+            );
+          },
+        );
+      }).toList(),
+    );
+  }
+}
+
+class VideoWidget extends StatefulWidget {
+  const VideoWidget({Key? key, required this.videoUrl}) : super(key: key);
+  final String videoUrl;
+
+  @override
+  State<VideoWidget> createState() => _VideoWidgetState(videoUrl);
+}
+
+class _VideoWidgetState extends State<VideoWidget> {
+  late VideoPlayerController _controller;
+  final String videoUrl;
+
+  _VideoWidgetState(this.videoUrl);
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset(videoUrl)
+      ..initialize().then((_) {
+        _controller.play();
+        setState(() {});
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return VideoPlayer(_controller);
+  }
+}
+
+class PostContent extends StatelessWidget {
+  const PostContent({
+    Key? key,
+    required this.comment,
+    required this.likeCount,
+    required this.commentCount,
+    required this.shareCount,
+    required this.avatarImage,
+    required this.music,
+  }) : super(key: key);
+  final String comment;
+  final String likeCount;
+  final String commentCount;
+  final String shareCount;
+  final String avatarImage;
+  final String music;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 100,
+          padding: EdgeInsets.only(top: 40),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Following",
+                style: TextStyle(
+                  color: Colors.white54,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(width: 20),
+              const Text(
+                "For you",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: Row(
+            children: [
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.only(bottom: 80, left: 10),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        comment,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.music_note,
+                            color: Colors.white,
+                            size: 15,
+                          ),
+                          SizedBox(width: 5),
+                          Text(
+                            music,
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                width: 80,
+                padding: EdgeInsets.only(bottom: 50),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Container(
+                      height: 80,
+                      child: Stack(
+                        alignment: AlignmentDirectional.bottomCenter,
+                        children: [
+                          Container(
+                            margin: EdgeInsets.all(5),
+                            child: CircleAvatar(
+                              radius: 25,
+                              backgroundImage: AssetImage(avatarImage),
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.all(2),
+                            decoration: BoxDecoration(
+                              color: Colors.red,
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Icon(
+                              Icons.add,
+                              color: Colors.white,
+                              size: 15,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: 80,
+                      child: Column(
+                        children: [
+                          Icon(
+                            Icons.favorite,
+                            color: Colors.white.withOpacity(0.85),
+                            size: 45,
+                          ),
+                          Text(
+                            likeCount,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: 80,
+                      child: Column(
+                        children: [
+                          Icon(
+                            Icons.comment,
+                            color: Colors.white.withOpacity(0.85),
+                            size: 45,
+                          ),
+                          Text(
+                            commentCount,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: 80,
+                      child: Column(
+                        children: [
+                          Icon(
+                            Icons.share,
+                            color: Colors.white.withOpacity(0.85),
+                            size: 45,
+                          ),
+                          Text(
+                            shareCount,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: 80,
+                      child: Column(
+                        children: [
+                          CircleAvatar(
+                            backgroundImage:
+                            AssetImage("assets/images/photo2.png"),
+                            backgroundColor: Colors.white.withOpacity(0),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
